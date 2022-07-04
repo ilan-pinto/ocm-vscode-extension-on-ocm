eb2bf3788d5775a7887ad5eb64c68301d7547dac
Ilan Pinto <ilpinto@redhat.com>
2022-07-04 10:47:59 +0300

    react web view

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,13 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/no-var-requires */
 import * as createEnvironment from './commands/createEnvironment';
 import * as newProject from './commands/newProject';
+import * as path from 'path';
 import * as verifyEnvironment from './commands/verifyEnvironment';
 import * as vscode from 'vscode';
+
+
+
 import { ConnectedClustersProvider } from './providers/connectedClusters';
 
 export function activate(context: vscode.ExtensionContext): void {
@@ -16,6 +22,134 @@ export function activate(context: vscode.ExtensionContext): void {
 		vscode.window.registerTreeDataProvider(
 			'ocm-vscode-extension.connectedClustersView', connectedClustersProvider),
 		vscode.commands.registerCommand(
-			'ocm-vscode-extension.connectedClustersView.refresh', () => connectedClustersProvider.refresh())
+			'ocm-vscode-extension.connectedClustersView.refresh', () => connectedClustersProvider.refresh()),
+		vscode.commands.registerCommand('ocm-vscode-extension.demoView', () => {
+				ReactPanel.createOrShow(context.extensionPath);
+			})
 	);
+}
+
+
+
+
+class ReactPanel {
+	/**
+	 * Track the currently panel. Only allow a single panel to exist at a time.
+	 */
+	public static currentPanel: ReactPanel | undefined;
+
+	private static readonly viewType = 'react';
+
+	private readonly _panel: vscode.WebviewPanel;
+	private readonly _extensionPath: string;
+	private _disposables: vscode.Disposable[] = [];
+
+	public static createOrShow(extensionPath: string): void {
+		const column = vscode.window.activeTextEditor ? vscode.window.activeTextEditor.viewColumn : undefined;
+
+		// If we already have a panel, show it.
+		// Otherwise, create a new panel.
+		if (ReactPanel.currentPanel) {
+			ReactPanel.currentPanel._panel.reveal(column);
+		} else {
+			ReactPanel.currentPanel = new ReactPanel(extensionPath, column || vscode.ViewColumn.One);
+		}
+	}
+
+	private constructor(extensionPath: string, column: vscode.ViewColumn) {
+		this._extensionPath = extensionPath;
+
+		// Create and show a new webview panel
+		this._panel = vscode.window.createWebviewPanel(ReactPanel.viewType, "React", column, {
+			// Enable javascript in the webview
+			enableScripts: true,
+
+			// And restric the webview to only loading content from our extension's `media` directory.
+			localResourceRoots: [
+				vscode.Uri.file(path.join(this._extensionPath, 'build'))
+			]
+		});
+		
+		// Set the webview's initial html content 
+		this._panel.webview.html = this._getHtmlForWebview();
+
+		// Listen for when the panel is disposed
+		// This happens when the user closes the panel or when the panel is closed programatically
+		this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+
+		// Handle messages from the webview
+		this._panel.webview.onDidReceiveMessage(message => {
+			switch (message.command) {
+				case 'alert':
+					vscode.window.showErrorMessage(message.text);
+					return;
+			}
+		}, null, this._disposables);
+	}
+
+	public doRefactor(): void {
+		// Send a message to the webview webview.
+		// You can send any JSON serializable data.
+		this._panel.webview.postMessage({ command: 'refactor' });
+	}
+
+	public dispose(): void {
+		ReactPanel.currentPanel = undefined;
+
+		// Clean up our resources
+		this._panel.dispose();
+
+		while (this._disposables.length) {
+			const x = this._disposables.pop();
+			if (x) {
+				x.dispose();
+			}
+		}
+	}
+
+	private _getHtmlForWebview(): string {
+		const manifest = require(path.join(this._extensionPath, 'build', 'asset-manifest.json'));
+		const mainScript = manifest.files['main.js'];
+		const mainStyle = manifest.files['main.css'];
+		const mediaFile = manifest.files['static/media/logo.svg']; 
+
+		const scriptPathOnDisk = vscode.Uri.file(path.join(this._extensionPath, 'build', mainScript));
+		const scriptUri = this._panel.webview.asWebviewUri(scriptPathOnDisk);
+		const stylePathOnDisk = vscode.Uri.file(path.join(this._extensionPath, 'build', mainStyle));
+		const styleUri = this._panel.webview.asWebviewUri(stylePathOnDisk);
+		const mediaPathOnDisk = vscode.Uri.file(path.join(this._extensionPath, 'build', mediaFile));
+		const mediaUri = this._panel.webview.asWebviewUri(mediaPathOnDisk);
+
+		// Use a nonce to whitelist which scripts can be run
+		const nonce = getNonce();
+
+		return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="utf-8">
+				<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+				<meta name="theme-color" content="#000000">
+				<title>React App</title>
+				<link rel="stylesheet" type="text/css" href="${styleUri}">
+				<base href="${vscode.Uri.file(path.join(this._extensionPath, 'build/static')).with({ scheme: 'vscode-resource' })}/">
+			</head>
+
+			<body>
+				<noscript>You need to enable JavaScript to run this app.</noscript>
+				<img src=${mediaUri} className="App-logo" alt="logo" />
+				<div id="root"></div>
+				
+				<script nonce="${nonce}" src="${scriptUri}"></script>
+			</body>
+			</html>`;
+	}
+}
+
+function getNonce(): string {
+	let text = "";
+	const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	for (let i = 0; i < 32; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
 }
